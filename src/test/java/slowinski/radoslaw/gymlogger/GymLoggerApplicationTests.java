d6d10f213b7c11b0a9f16e01542941683698131a
ares97 <rslowinski97@gmail.com>
2017-12-26 15:29:02 +0100

    Initial commit

---
+++ src/test/java/slowinski/radoslaw/gymlogger/GymLoggerApplicationTests.java
@@ -0,0 +1,16 @@
+package slowinski.radoslaw.gymlogger;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GymLoggerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
