ec66ac8c9e33ded3a6deded64551b4d4c11aebd8
ares97 <rslowinski97@gmail.com>
2017-12-27 11:30:24 +0100

    sample data inserted

--- src/main/java/slowinski/radoslaw/gymlogger/GymLoggerApplication.java
+++ src/main/java/slowinski/radoslaw/gymlogger/GymLoggerApplication.java
@@ -1,13 +1,20 @@
 package slowinski.radoslaw.gymlogger;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import slowinski.radoslaw.gymlogger.exercise.entity.Exercise;
+import slowinski.radoslaw.gymlogger.exercise.entity.ExerciseLog;
+import slowinski.radoslaw.gymlogger.exercise.entity.SeriesLog;
+import slowinski.radoslaw.gymlogger.exercise.repository.ExerciseLogRepository;
 import slowinski.radoslaw.gymlogger.exercise.repository.ExerciseRepository;
+import slowinski.radoslaw.gymlogger.exercise.repository.SeriesLogRepository;
 import slowinski.radoslaw.gymlogger.user.entity.User;
 import slowinski.radoslaw.gymlogger.user.service.UserService;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class GymLoggerApplication implements CommandLineRunner {
@@ -20,7 +27,10 @@ public class GymLoggerApplication implements CommandLineRunner {
     UserService userService;
     @Autowired
     ExerciseRepository exerciseRepository;
-
+    @Autowired
+    ExerciseLogRepository exerciseLogRepository;
+    @Autowired
+    SeriesLogRepository seriesLogRepository;
     @Override
     public void run(String... args) {
         User user = new User("admin", "password", "ROLE_USER");
@@ -30,5 +40,22 @@ public class GymLoggerApplication implements CommandLineRunner {
         exercise.setDescription("description...");
         exercise.setTitle("Military Press");
         exerciseRepository.save(exercise);
+
+        ExerciseLog exerciseLog = new ExerciseLog();
+        exerciseLog.setExercise(exercise);
+
+        SeriesLog seriesLog = new SeriesLog();
+        seriesLog.setReps(5);
+        seriesLog.setWeight(92.5f);
+        SeriesLog seriesLog2 = new SeriesLog();
+        seriesLog2.setReps(4);
+        seriesLog2.setWeight(95.0f);
+        seriesLogRepository.save(seriesLog);
+        seriesLogRepository.save(seriesLog2);
+        
+        exerciseLog.setSeriesLogs(Arrays.asList(seriesLog, seriesLog2));
+        exerciseLogRepository.save(exerciseLog);
+
+
     }
 }
