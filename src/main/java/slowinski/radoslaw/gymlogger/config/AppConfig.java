f6afa474a15e4e61d13269e329a20c64c8f2b45e
ares97 <slowinskir@wp.pl>
2018-01-10 15:54:36 +0100

    prettier LocalDate json mapper used

--- src/main/java/slowinski/radoslaw/gymlogger/config/AppConfig.java
+++ src/main/java/slowinski/radoslaw/gymlogger/config/AppConfig.java
@@ -1,9 +1,13 @@
 package slowinski.radoslaw.gymlogger.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import slowinski.radoslaw.gymlogger.workout.service.TrainingFacade;
 import slowinski.radoslaw.gymlogger.workout.service.impl.TrainingFacadeImpl;
+
 
 @Configuration
 public class AppConfig {
@@ -11,5 +15,16 @@ public class AppConfig {
     @Bean
     public TrainingFacade trainingFacade() {
         return new TrainingFacadeImpl();
+    }
+
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
+    }
+
+    @Bean
+    public ObjectWriter objectWriter() {
+        return objectMapper().writerWithDefaultPrettyPrinter();
     }
 }
